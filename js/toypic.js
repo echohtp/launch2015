e0783ffa2f3a5e6cd2400385955ed1114a9c7f22
Jason Schorr <echohtp@gmail.com>
2015-02-27 18:13:33 -0800

    some new crap

--- js/toypic.js
+++ js/toypic.js
@@ -16,5 +16,32 @@ var toyCollection = new mCollection();
 
 var fbRef = new Firebase('https://toypic.firebaseio.com/');
 
+var current_keywords = [];
 
+$(function(){
 
+	//setup event handlers
+	$('#btn_add_keyword').click(function(event){
+		event.preventDefault();
+		console.log('keyword btn clikced');
+
+		if ( $('#input_keyword').val() !== '' ){
+			console.log('we have a keyword: ' + $('#input_keyword').val() );
+			
+			var keyword = $('#input_keyword').val();
+			$('#input_keyword').val('');
+
+			current_keywords.push(keyword);
+
+			var template = _.template( $('#template_keywords_list').html() );
+			var html = template( current_keywords );
+
+			$('#selected_keywords').html(html);
+		}
+	});
+
+	$('#range_price').change(function(event){
+		$('#badge_price').text('$' + $('#range_price').val() );
+	});
+
+});
