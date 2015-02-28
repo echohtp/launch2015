4092d2ac3301bf88690afd6fc74d09065b01cea6
Sean Lars Ganser <sean.ganser@gmail.com>
2015-02-28 10:56:41 -0800

    require deps all set

--- js/index.js
+++ js/index.js
@@ -1,4 +1,31 @@
-require(['lib/jquery.min','lib/underscore','lib/backbone','lib/firebase', 'lib/bootstrap.min'],function(){
+require.config({
+    baseUrl: 'js',
+    paths: {
+        // the left side is the module ID,
+        // the right side is the path to
+        // the jQuery file, relative to baseUrl.
+        // Also, the path should NOT include
+        // the '.js' file extension. This example
+        // is using jQuery 1.9.0 located at
+        // js/lib/jquery-1.9.0.js, relative to
+        // the HTML page.
+        'jquery': 'lib/jquery.min',
+		'underscore': 'lib/underscore',
+		'backbone': 'lib/backbone',
+		'firebase': 'lib/firebase', 
+		'bootstrap':'lib/bootstrap.min'
+    },
+    shim:{
+    	'bootstrap':{
+    		deps:['jquery']
+    	},
+    	'backbone':{
+    		deps:['underscore']
+    	}
+    }
+});
+
+require(['jquery','underscore','backbone','bootstrap','firebase'],function(){
 
 	console.log ('toypic started');
 
